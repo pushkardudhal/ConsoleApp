c09bb0c9d653b8da92f848ec0a188de272879490
pushkardudhal <pushkar.dudhal@hotmail.com>
2017-12-24 02:22:39 +0530

    Included files of csharpbasics project

---
+++ CsharpBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasics
+{
+    class Program
+    {       
+
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            for (i = 1; i <= 7; i++)
+            {
+                for (j = 1; j <= i; ++j)
+                    Console.Write(j);
+
+                for (k = 7 - i; k >= 1; k--)
+                    Console.Write("*");
+
+                Console.Write("\n");
+            }
+                Console.ReadLine();
+        }
+    }
+}
